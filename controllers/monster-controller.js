9dc934a6110506bd4e9d893aba54ebfe24b4371b
Rancor38 <zak.rancor@hotmail.com>
2023-01-17 20:38:39 -0800

    bruh

---
+++ controllers/monster-controller.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const router = express.Router()
+
+// import model (Monster)
+const { Monster } = require('../models')
+
+const db = require('../models') // db.Monster 
+// const Monster = require('../models/Monster')
+
+console.log(Monster)
+
+// Routes
+// http://localhost:4000/Monster/
+router.get('/', async (req,res)=> {
+    // res.status(200).json({message: "Monster index/get route"})
+    try {
+        const allMonster = await Monster.find({})
+        res.status(200).json(allMonster)
+    } catch (err){
+        res.status(400).json({error: err})
+    }
+})
+
+// http://localhost:4000/Monster/
+router.post('/', async (req,res)=> {
+    // console.log('post route', req.body)
+    // res.status(201).json({message: "Monster create/post route"})
+
+    try {
+        // 
+        const newMonster = await Monster.create(req.body)
+        res.status(201).json(newMonster)
+
+    } catch (err) {
+        res.status(400).json({ error: err })
+    }
+})
+
+// http://localhost:4000/Monster/:id - GET
+router.get('/:id', async (req,res)=> {
+    // res.status(200).json({message: "Monster show/get route /Monster/"+req.params.id})
+    try {
+
+        const foundMonster = await Monster.findById(req.params.id)
+        res.status(200).json(foundMonster)
+
+    }catch (err) {
+        res.status(400).json({error: err})
+    }
+})
+// http://localhost:4000/Monster/name - GET
+router.post('/name/:monsterName', async (req,res)=> {
+    // res.status(200).json({message: "Monster index/get route"})
+    try {
+        const namedMonster = await Monster.find({
+            // regex keyword makes this work, by returning the closest results
+            $name: req.params.monsterName
+        }).exec()
+        console.log((namedMonster))
+        res.status(200).json(namedMonster)
+    } catch (err){
+        res.status(400).json({error: err})
+    }
+})
+// http://localhost:4000/Monster/:id - DELETE
+router.delete('/:id', async (req,res)=> {
+    // res.status(200).json({message: "Monster destroy/delete route /Monster/"+req.params.id})
+    try {
+        const deletedMonster = await Monster.findByIdAndDelete(req.params.id)
+        res.status(200).json(deletedMonster)
+
+    }catch (err) {
+        // console.log(err)
+        res.status(400).json({error: err})
+    }
+})
+
+// http://localhost:4000/Monster/:id - PUT
+router.put('/:id', async (req,res)=> {
+    // res.status(200).json({message: "Monster update/put route /Monster/"+req.params.id})
+    try {
+
+        const updatedMonster = await Monster.findByIdAndUpdate(req.params.id, req.body, {new:true})
+        res.status(200).json(updatedMonster)
+
+    }catch (err) {
+        res.status(400).json({error: err})
+    }
+})
+
+module.exports = router 
---
+++ run.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+// app dependencies
+const cors = require('cors')
+const morgan = require('morgan')
+
+// controller imports
+const monsterController = require('./controllers/monster-controller')
+
+require('dotenv').config()
+require('./config/db.connection') // node runs all of the code in db.connection
+
+const { PORT } = process.env  
+
+// express / app middleware
+app.use(express.json())
+
+// cors helper function
+app.use(cors()) // allows for cross origin request - open channel 
+// morgan request logger (for dev)
+app.use(morgan('dev'))
+// router middleware
+app.use('/monster', monsterController)
+
+// root - home / index route for api - redirects to the monster index route 
+app.get('/', (req,res)=>res.redirect('/monster'))
+
+app.listen(PORT, ()=>console.log(`Listening on port: ${PORT}`))
